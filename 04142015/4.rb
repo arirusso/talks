2a30a34e1c0c7cd7f5c9ff21504273149e0e62ed
Ari Russo <ari.russo@gmail.com>
2015-04-13 23:14:16 -0400

    comments

--- 04142015/4.rb
+++ 04142015/4.rb
@@ -5,12 +5,17 @@ require "pp"
 require "midi-message" # https://github.com/arirusso/midi-message
 require "unimidi" # https://github.com/arirusso/unimidi
 
+# Select an output
 @output = UniMIDI::Output.gets
 
+# Create a note on message
 @message = MIDIMessage::NoteOn.new(0x0, 64, 100)
 
+# Output the note on message
 @output.puts(*@message.to_a)
 
+# Create a note off from the note on
 @off = @message.to_note_off
 
+# Output the note off
 @output.puts(*@off.to_a)
