0d3aee39cf5246333c3fbe943cd3c67e20face46
Ari Russo <ari.russo@gmail.com>
2015-04-13 21:43:45 -0400

    use nibble/nibble byte byte notation

--- 04142015/5.rb
+++ 04142015/5.rb
@@ -9,9 +9,9 @@ nibbler.parse("90")
 nibbler.parse("40")
 nibbler.parse("40")
 nibbler.parse("904040")
-nibbler.parse(0x90, 0x40, 0x40)
+nibbler.parse(0x90, 64, 100)
 
 # Alternatively use the Midilib MIDI message library
 midilib_nibbler = Nibbler.new(:message_lib => :midilib)
 
-midilib_nibbler.parse("9", "0", 0x40, "40")
+midilib_nibbler.parse("9", "0", 64, 64)
