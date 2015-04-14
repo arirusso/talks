817365fcc7ef1b7bac187937d81d6f57bbb2044e
Ari Russo <ari.russo@gmail.com>
2015-04-13 23:42:58 -0400

    comments

--- 04142015/11.rb
+++ 04142015/11.rb
@@ -4,26 +4,31 @@
 require "diamond" # https://github.com/arirusso/diamond
 require "unimidi" # https://github.com/arirusso/unimidi
 
+# Select a MIDI output
 @output = UniMIDI::Output.gets
 
 options = {
-  :gate => 90,
-  :interval => 7,
-  :midi => @output,
-  :pattern => "UpDown",
-  :range => 4,
-  :rate => 8,
-  :tx_channel => 1
+  :midi => @output, # Output notes to the MIDI output
+  :gate => 90, # How long the notes ring out
+  :pattern => "UpDown", # Which arpeggiation algorithm to use
+  :interval => 7, # Argument to provide an interval for the arpeggiation algorithm
+  :range => 4, # Argument for how many octaves to apply to arpeggiation algorithm for
+  :rate => 8, # How to divide the beat
+  :tx_channel => 1 # What MIDI channel to publish to
 }
 
+# Create a clock with tempo of 101 bpm
 @clock = Diamond::Clock.new(101)
 
+# Instantiate the arpeggiator
 @arpeggiator = Diamond::Arpeggiator.new(options)
 
+# Make the clock aware of the arpeggiator
 @clock << @arpeggiator
 
+# Feed the arpeggiator/pattern algorithm notes
 @chord = ["C1", "G1", "Bb2", "A3"]
-
 @arpeggiator.add(*@chord)
 
-@clock.start
+# Start the arpeggiator
+@clock.start(:background => true)
