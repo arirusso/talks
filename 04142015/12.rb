537bed120e8e47059ad71cde59a8f683ba4393b6
Ari Russo <ari.russo@gmail.com>
2015-04-13 23:43:07 -0400

    comments

--- 04142015/12.rb
+++ 04142015/12.rb
@@ -1,12 +1,14 @@
 # Sync multiple Arpeggiators to each other
-# this is meant to be used in the repl
+# (this is meant to be used in the repl)
 
 require "diamond" # https://github.com/arirusso/diamond
 require "unimidi" # https://github.com/arirusso/unimidi
 
+# Select MIDI IO
 @input = UniMIDI::Input.gets
 @output = UniMIDI::Output.gets
 
+# Add a custom arpeggiation pattern based on Fibonacci function
 def fibonacci(n)
   n = fibonacci( n - 1 ) + fibonacci( n - 2 ) if n > 1
   n
@@ -16,32 +18,34 @@ Diamond::Pattern.add("fibonacci") { |range, interval| 0.upto(range).map { |n| fi
 
 # Basic options
 options = {
-  :gate => 90,
-  :interval => 7,
-  :midi => @output,
-  :pattern => "UpDown",
-  :range => 2,
-  :rate => 8
+  :midi => @output, # Use the MIDI output
+  :gate => 90, # How long the notes ring out for
+  :pattern => Diamond::Pattern.find("fibonacci"), # Use the Fibonacci pattern
+  :range => 2, # The range argument for the Fibonacci arpeggiation function (how many octaves)
+  :interval => 7, # The interval argument for the Fibonacci arpeggiation function
+    # (The interval is ignored by the fibonacci algorithm, but if we switched patterns we would want
+    # that argument available)
 }
 
-@arpeggiators = 2.times.map do |i|
+# Instantiate some arpeggiators
+@arpeggiators = 3.times.map do |i|
   arpeggiator = Diamond::Arpeggiator.new(options)
+  # Mutate the options for each one
   arpeggiator.tx_channel = i
   arpeggiator.transpose(i * 12)
   arpeggiator.range += i
   arpeggiator.rate = 4 * (2 ** i)
-  arpeggiator.pattern = Diamond::Pattern.find("fibonacci")
   arpeggiator
 end
 
 # A clock to control all of the arpeggiators
 @clock = Diamond::Clock.new(@input, :midi => @output)
 
+# Make the clock aware of the arpeggiators
 @clock << @arpeggiators
 
-# Some notes..
+# Give the arpeggiators some notes
 @chord = ["C0", "G0", "Bb0", "A2"]
-
 @arpeggiators.each { |arpeggiator| arpeggiator << @chord }
 
 # Start the clock
